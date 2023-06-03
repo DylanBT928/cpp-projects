172624a6a4f6452b194ec50bd1420c2c4440496a
Dylan Tran <75633077+DylanBT928@users.noreply.github.com>
2023-06-03 15:26:28 -0700

    Update and rename calculator.cpp to Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -2,12 +2,14 @@
 #include <cmath>
 using namespace std;
 int main(){
+    //Variables to allow the user to choose math operation and numbers
     char op;
     double num1, num2;
     cout<<"Enter two numbers: "<<endl;
     cin>>num1>>num2;
     cout<<"Choose an operation: \"+, -, *, /, ^\""<<endl;
     cin>>op;
+    //Switch statement to choose which operation to use, depending on what user sets op equal to
     switch(op){
         case '+':
             cout<<num1<<" + "<<num2<<" = "<<num1+num2;
@@ -25,6 +27,7 @@ int main(){
             cout<<num1<<" ^ "<<num2<<" = "<<pow(num1,num2);
             break;
         default:
+            //Prints this if op is not equal to any of the previous cases
             cout<<"Unknown operator. Please try again.";
     }
     return 0;
