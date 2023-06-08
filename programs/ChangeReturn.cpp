ade6d2ffe5fefe8c3cb39cc263270d7cb3375d4b
Dylan Tran <dylanbt928@gmail.com>
2023-06-08 15:32:27 -0700

    Update ChangeReturn.cpp

--- programs/ChangeReturn.cpp
+++ programs/ChangeReturn.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int cost;
+    double cost;
     string price;
     int quarters, dimes, nickels, pennies;
-    cout<<"Enter the cost in pennies: ";
+    cout<<"Enter the cost: ";
     cin>>cost;
     //Finds how many of each coin can go into the cost
-    quarters = cost/25;
-    cost%=25;
-    dimes = cost/10;
-    cost%=10;
-    nickels = cost/5;
-    cost%=5;
-    pennies = cost;
+    quarters = cost/0.25;
+    cost-=quarters*0.25;
+    dimes = cost/0.10;
+    cost-=dimes*0.10;
+    nickels = cost/0.05;
+    cost-=nickels*0.05;
+    pennies = cost*100;
     //Returns amount of change
     cout<<"You need "<<quarters<<" quarters, "<<dimes<<" dimes, "<<nickels<<" nickels, and "<<pennies<<" pennies.";
     return 0;
