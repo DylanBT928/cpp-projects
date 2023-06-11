3ecc6dc70724c168861a37413a528700c8ec1d29
Dylan Tran <dylanbt928@gmail.com>
2023-06-10 20:27:18 -0700

    Update Hangman.cpp

--- projects/Hangman/Hangman.cpp
+++ projects/Hangman/Hangman.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
+string secretWord{};
+char letter{};
+
+string guess(){
+
+}
+
+string reveal(){
+
+}
 
 int main(){
-
+    cout<<"Please enter a word: ";
+    cin>>secretWord;
 
     return 0;
 }
