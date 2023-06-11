2e327a29554ec452ddfb23484bf0b206ba240e09
Dylan Tran <dylanbt928@gmail.com>
2023-06-10 23:10:00 -0700

    Update Hangman.cpp

--- projects/Hangman/Hangman.cpp
+++ projects/Hangman/Hangman.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 using namespace std;
-string secretWord{};
+string secretWord{}, hidden{}, usedLetters{};
 char letter{};
 
-string hide(){
+//Converts the secret word into a blank
+string hide(string secret){
 
 }
 
-string reveal(){
+//Checks if the letter is in the word
+bool isValid(char l){
+    
+}
+
+//Reveals the letter(s) in the word
+string reveal(char l){
 
 }
 
 int main(){
+    int guesses = 6;
     cout<<"Please enter a word: ";
     cin>>secretWord;
-
+    hidden = hide(secretWord);
+    while(guesses>0 && secretWord!=hidden){
+        cout<<"Guess a letter: ";
+        cin>>letter;
+        if(isValid(letter)){
+            cout<<reveal(letter)<<'\n';
+        } else {
+            guesses--;
+        }
+    }
+    if(secretWord==hidden){
+        cout<<"Congratulations, you win!"<<endl;
+    } else {
+        cout<<"Sorry, you lost."<<endl;
+    }
     return 0;
 }
