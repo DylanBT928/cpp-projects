a4b7cdb4f1a558b6ff20c825dea99a992b27c92a
Dylan Tran <dylanbt928@gmail.com>
2023-07-01 22:06:22 -0700

    Added functions for GoFish.cpp

--- projects/Go Fish/GoFish.cpp
+++ projects/Go Fish/GoFish.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
+#include <vector>
 #include <ctime>
 using namespace std;
 string deck[52] = {"Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",
                    "Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",
                    "Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",
                    "Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",};
+int bookmark{0};
 void shuffle(){
     srand(static_cast<unsigned int>(time(nullptr)));
     for(int i = 0; i<52; i++){
         swap(deck[i], deck[rand()%52]);
     }
 }
+void draw(vector<string> hand){
+    hand.push_back(deck[bookmark]);
+    bookmark++;
+}
+bool isGameOver(){
+    return bookmark>52;
+}
 int main(){
-    shuffle();
+    //shuffle();
+    vector<string> playerHand;
+    vector<string> dealerHand;
+    for(int i = 0; i<7; i++){
+        draw(playerHand);
+        draw(dealerHand);
+    }
+    while(!isGameOver){
+        
+    }
 }
