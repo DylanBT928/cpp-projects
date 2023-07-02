0cbcf95765c76f87884439025b749c6550a34250
Dylan Tran <dylanbt928@gmail.com>
2023-07-01 21:09:40 -0700

    Create GoFish.cpp

---
+++ projects/Go Fish/GoFish.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main(){
+    string deck[52] = {"Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",
+                   "Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",
+                   "Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",
+                   "Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",};
+}
