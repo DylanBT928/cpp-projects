b801a5eab2efdbb18332b6c20fee1e0a23b15750
Dylan Tran <dylanbt928@gmail.com>
2023-07-01 21:34:57 -0700

    Shuffles deck for GoFish.cpp

--- projects/Go Fish/GoFish.cpp
+++ projects/Go Fish/GoFish.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
-int main(){
-    string deck[52] = {"Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",
+string deck[52] = {"Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",
                    "Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",
                    "Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",
                    "Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",};
+void shuffle(){
+    srand(static_cast<unsigned int>(time(nullptr)));
+    for(int i = 0; i<52; i++){
+        swap(deck[i], deck[rand()%52]);
+    }
+}
+int main(){
+    shuffle();
 }
