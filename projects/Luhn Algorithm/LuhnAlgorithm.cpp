f42a8bb5024708d812bc66b1d708622dd01a8fc1
Dylan Tran <dylanbt928@gmail.com>
2023-07-01 17:55:48 -0700

    Create LuhnAlgorithm.cpp

---
+++ projects/Luhn Algorithm/LuhnAlgorithm.cpp
@@ -0,0 +1,5 @@
+/* This program uses the Luhn algorithm (aka the Modulus 10 
+ * algorithm) to determine whether the identification number 
+ * provided by a user is accurate. The algorithm is most 
+ * notably used in credit cards, IMEI, or SSN.
+ */
