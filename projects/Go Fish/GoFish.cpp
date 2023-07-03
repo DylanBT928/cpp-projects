192ceec5b8f4f7c03dd66b99ad726cf69fdc861e
Dylan Tran <dylanbt928@gmail.com>
2023-07-02 23:33:57 -0700

    Added showHand() and guessing for GoFish.cpp

--- projects/Go Fish/GoFish.cpp
+++ projects/Go Fish/GoFish.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 #include <ctime>
 using namespace std;
-string deck[52] = {"Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",
-                   "Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",
-                   "Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",
-                   "Ace","1","2","3","4","5","6","7","8","9","Jack","Queen","King",};
+string deck[52] = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King",
+                   "Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King",
+                   "Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King",
+                   "Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King",};
 int bookmark{0};
 void shuffle(){
     srand(static_cast<unsigned int>(time(nullptr)));
@@ -13,22 +13,50 @@ void shuffle(){
         swap(deck[i], deck[rand()%52]);
     }
 }
-void draw(vector<string> hand){
-    hand.push_back(deck[bookmark]);
+void draw(vector<string> hand, int card){
+    hand.push_back(deck[card]);
     bookmark++;
+}
+void showHand(vector<string> hand){
+    cout<<hand[0];
+    for(int i = 1; i<hand.size(); i++){
+        cout<<", "<<hand[i];
+    }
+    cout<<'\n';
 }
 bool isGameOver(){
     return bookmark>52;
 }
 int main(){
-    //shuffle();
     vector<string> playerHand;
     vector<string> dealerHand;
-    for(int i = 0; i<7; i++){
-        draw(playerHand);
-        draw(dealerHand);
+    shuffle();
+    for(int i = 0; i<14; i+=2){
+        draw(playerHand,i);
+        draw(dealerHand,i++);
     }
     while(!isGameOver){
-        
+        int guess{};
+        showHand(playerHand);
+        cout<<"Guess a card: ";
+        cin>>guess;
+
+
+
+
+
+
+
+
+
+
+
+
+        cout<<"The deck has "<<(52-bookmark)<<" cards left.\n";
+
+
+
+
+        return 0;
     }
 }
