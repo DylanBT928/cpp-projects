b6f3dc6df126a5b684cd998b395e080db09b3cad
Dylan Tran <dylanbt928@gmail.com>
2023-09-27 14:17:36 -0700

    Add alphabet to CaesarCipher.cpp

--- programs/CaesarCipher.cpp
+++ programs/CaesarCipher.cpp
@@ -2,8 +2,9 @@
 #include <string>
 
 int main(){
+    char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                         'n','o','p','q','r','s','t','u','v','w','x','y','z'};
     
-
 
     return 0;
 }
