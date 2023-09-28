00e7441bf231763f35f52c81b326a10d054e3e96
Dylan Tran <dylanbt928@gmail.com>
2023-09-27 17:01:20 -0700

    Added options to CaesaeCipher.cpp

--- programs/CaesarCipher.cpp
+++ programs/CaesarCipher.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 
 int main(){
     char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
                          'n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    
+    std::string option{};
+    std::cout<<"encrypt or decrypt?\n";
+    std::cin>>option;
+
+    if(option.compare("encrypt") == 0){
+        //Encryption
+
+
+
+
+    } else if(option.compare("decrypt") == 0){
+        //Decryption
+
+
+
+
+    } else {
+        std::cout<<"Invalid option."<<std::endl;
+    }
+
+
+
+
+
 
     return 0;
 }
