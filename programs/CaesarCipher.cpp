a8bcc7410e17981de76c9123ee864df5b49a9914
Dylan Tran <dylanbt928@gmail.com>
2023-09-29 22:09:13 -0700

    Change to switch statement for CaesarCipher.cpp

--- programs/CaesarCipher.cpp
+++ programs/CaesarCipher.cpp
@@ -5,45 +5,45 @@ using namespace std;
 int main(){
     char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
                          'n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    string option{}, message{}, newmsg{};
-    int shift{3};
-    cout<<"encrypt or decrypt: ";
+    string message{}, newmsg{};
+    int option{1}, shift{3};
+    cout<<"(1)encrypt or (2)decrypt: ";
     cin>>option;
     cout<<"enter shift value: ";
     cin>>shift;
     cout<<"enter message: ";
     cin>>message;
 
-    if(option.compare("encrypt") == 0){
-        //Encryption
-    	cout<<"encrypting...\n";
-        for(int i = 0; i<message.length(); i++){
-            int j = 0;
-            while(message.charAt(i)!=alphabet[j] && j<26){
-                j++;
-                newmsg+=alphabet[j];
+    switch(option){
+        case 1:
+            //Encryption
+            cout<<"encrypting...\n";
+            for(int i = 0; i<message.length(); i++){
+                int j = 0;
+                while(message.at(i)!=alphabet[j] && j<26){
+                    j++;
+                }
+                if(isalpha(message.at(i))){
+                    newmsg+=alphabet[j];
+                } else {
+                    newmsg+=message[i];
+                }
+                j = 0;
             }
-            if(!isalpha(message.charAt(i))){
-                newmsg+=message[i];
-            }
-            j = 0;
-        }
-
-
-    } else if(option.compare("decrypt") == 0){
-        //Decryption
-        cout<<"decrypting...\n";
+            cout<<newmsg;
+            break;
+        case 2:
+            //Decryption
+            cout<<"decrypting...\n";
 
 
 
-    } else {
-        cout<<"Invalid option."<<endl;
+
+
+            break;
+        default:
+            cout<<"Invalid option."<<endl;
+            break;
     }
-
-
-
-
-
-
     return 0;
 }
