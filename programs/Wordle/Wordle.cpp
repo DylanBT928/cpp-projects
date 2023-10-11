113687f22213d845837133741fdf66ee1e7aabf2
Dylan Tran <dylanbt928@gmail.com>
2023-10-11 09:47:42 -0700

    Added some functions to Wordle

--- programs/Wordle/Wordle.cpp
+++ programs/Wordle/Wordle.cpp
@@ -6,14 +6,75 @@
 #include <algorithm>
 #include <Windows.h>
 
-std::string getRandomWord(){
-  //random word from solutions.txt
+const int LENGTH = 5;
+
+std::vector<std::string> readSolutions(int length){
+  std::string line;
+  std::vector<std::string> vec;
+  std::ifstream fin;
+  fin.open("./programs/Wordle/Solutions.txt");
+  while(getline(fin, line)){
+    if(line.length() == length){
+      vec.push_back(line);
+    }
+  }
+  fin.close();
+  return vec;
+}
+
+std::vector<std::string> readGuesses(int length){
+  std::string line;
+  std::vector<std::string> vec;
+  std::ifstream fin;
+  fin.open("./programs/Wordle/Guesses.txt");
+  while(getline(fin, line)){
+    if(line.length() == length){
+      vec.push_back(line);
+    }
+  }
+  fin.close();
+  return vec;
+}
+
+std::string chooseRandomWord(std::vector<std::string> vec){
+  auto iterator = vec.begin();
+  int randomNum = rand()%vec.size();
+  advance(iterator, randomNum);
+  return iterator[0];
+}
+
+char getNthLetter(int n){
+  return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[n-1];
+}
+
+int getNumFromChar(char x){
+  return ((int)x)-64;
+}
+
+std::string checkWord(std::string text, std::string correct, int len){
+  std::string out;
+  for(int i = 0; i<len; i++){
+    char text_char = text[i];
+    char correct_char = text[i];
   
+  //continue here
+  
+  
+  
+  
+  }
+  return out;
 }
 
 
+
+
+
+
 int main(){
-
-
+  
+  
+  std::vector<std::string> allSolutions =  readSolutions(LENGTH);
+  
   return 0;
 }
