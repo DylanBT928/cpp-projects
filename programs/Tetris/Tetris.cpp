f027b2d15372970d3bd0445ce374234aec95ecba
Dylan Tran <dylanbt928@gmail.com>
2024-01-02 22:47:02 -0800

    Created a rotation function for tetrominoes

--- programs/Tetris/Tetris.cpp
+++ programs/Tetris/Tetris.cpp
@@ -5,6 +5,23 @@ using namespace std;
 
 wstring tetromino[7];
 
+int rotate(int px, int py, int r)
+{
+    switch (r % 4)
+    {
+        case 0:
+            return py * 4 + px;           // 0 degrees
+        case 1: 
+            return 12 + py - (px * 4);    // 90 degrees
+        case 2:
+            return 15 - (py * 4) - px;    // 180 degrees
+        case 3:
+            return 3 - py + (px * 4);     // 270 degrees
+        default:
+            return 0;
+    }
+}
+
 int main()
 {
     // Create assets
