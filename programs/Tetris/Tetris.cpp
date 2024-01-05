e4b2ac4ad963c7bb9bb09db1d50b184aca741da5
Dylan Tran <dylanbt928@gmail.com>
2024-01-04 20:50:45 -0800

    Created playing field and displayed each frame

--- programs/Tetris/Tetris.cpp
+++ programs/Tetris/Tetris.cpp
@@ -1,9 +1,19 @@
 #define UNICODE
 
 #include <iostream>
+#include <thread>
+#include <vector>
+#include <stdio.h>
+#include <Windows.h>
 using namespace std;
 
 wstring tetromino[7];
+int nFieldWidth = 12;
+int nFieldHeight = 18;
+unsigned char *pField = nullptr;
+
+int nScreenWidth = 120;   // Console screen size X (columns)
+int nScreenHeight = 40;  // Console screen size Y (rows)
 
 int rotate(int px, int py, int r)
 {
@@ -60,8 +70,30 @@ int main()
     tetromino[6].append(L".X..");
     tetromino[6].append(L".X..");
 
+    pField = new unsigned char[nFieldWidth * nFieldHeight]; // Create playing field buffer
+    for (int x = 0; x < nFieldWidth; x++) // Board boundary
+        for (int y = 0; y < nFieldHeight; y++)
+            pField[y * nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
 
+    wchar_t *screen = new wchar_t[nScreenWidth * nScreenHeight];
+    for (int i = 0; i < nScreenWidth * nScreenHeight; i++)
+        screen[i] = L' ';
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
 
+    bool bGameOver = false;
+
+    while (!bGameOver)
+    {
+        // Draw field
+        for (int x = 0; x < nFieldWidth; x++)
+            for (int y = 0; y < nFieldHeight; y++)
+                screen[(y + 2) * nScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y * nFieldWidth + x]];
+        
+        // Display frame
+        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0, 0 }, &dwBytesWritten);
+    }
 
     return 0;
 }
