3e86c71fa2f17b59ba3917141dc2929ca7ad99b0
Dylan Tran <dylanbt928@gmail.com>
2024-01-05 20:40:49 -0800

    Check if the piece fits and draws each piece

--- programs/Tetris/Tetris.cpp
+++ programs/Tetris/Tetris.cpp
@@ -32,6 +32,30 @@ int rotate(int px, int py, int r)
     }
 }
 
+bool doesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY)
+{
+    for (int px = 0; px < 4; px++)
+        for (int py = 0; py < 4; py++)
+        {
+            // Get index into piece
+            int pi = rotate(px, py, nRotation);
+
+            // Get index into field
+            int fi = (nPosY + py) * nFieldWidth + (nPosX + px);
+
+            if (nPosX + px >= 0 && nPosX + px < nFieldWidth)
+            {
+                if (nPosY + py >= 0 && nPosY + py < nFieldHeight)
+                {
+                    if (tetromino[nTetromino][pi] == L'X' && pField[fi] != 0)
+                        return false; // Fail on first hit
+                }
+            }
+        }
+
+    return true;
+}
+
 int main()
 {
     // Create assets
@@ -84,15 +108,38 @@ int main()
 
     bool bGameOver = false;
 
+    int nCurrentPiece = 0;
+    int nCurrentRotation = 0;
+    int nCurrentX = nFieldWidth / 2;
+    int nCurrentY = 0;
+
     while (!bGameOver)
     {
+        // Game timing
+
+
+        // Input
+
+
+        // Game logic
+
+
+        // Render output
+
+
         // Draw field
         for (int x = 0; x < nFieldWidth; x++)
             for (int y = 0; y < nFieldHeight; y++)
                 screen[(y + 2) * nScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y * nFieldWidth + x]];
         
+        // Draw current piece
+        for (int px = 0; px < 4; px++)
+            for (int py = 0; py < 4; py++)
+                if (tetromino[nCurrentPiece][rotate(px, py, nCurrentRotation)] == L'X')
+                    screen[(nCurrentY + py + 2) * nScreenWidth + (nCurrentX + px + 2)] = nCurrentPiece + 65;
+
         // Display frame
-        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0, 0 }, &dwBytesWritten);
+        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, {0, 0}, &dwBytesWritten);
     }
 
     return 0;
